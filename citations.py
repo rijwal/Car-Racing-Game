b5313bf5710755c9471342077605beac351e3537
rijwal <63560606+rijwal@users.noreply.github.com>
2023-07-28 17:20:21 -0600

    Add files via upload

---
+++ citations.py
@@ -0,0 +1,7 @@
+
+#https://www.geeksforgeeks.org/python-pandas-dataframe-mask/ used this to learn masks (collision detection)
+#https://stackoverflow.com (some ideas on how to get the car to move and turn)
+#https://www.youtube.com/watch?v=CYnL-_079J8 used this as an idea of collision and perfect collision detection
+#https://www.geeksforgeeks.org/python-classes-and-objects/ used this to undertand the class system and the __init__ function
+#https://www.w3schools.com/python/python_inheritance.asp used this to learn inheritance
+#https://www.programiz.com/python-programming/inheritance a great video used to understand class systems and inheritance
